1a18a73d9fff0303cc454dec507616b598ad2e80
ZEVEKE <zeveke.developing@gmail.com>
2017-06-07 20:36:35 +0300

    Common.PublicationInfo for Mobile

--- src/components/Common/PublicationInfo/Views/Mobile/tpl.js
+++ src/components/Common/PublicationInfo/Views/Mobile/tpl.js
@@ -23,39 +23,34 @@ function Mobile(props) {
     return (
         <section className={ Styles.publicationInfoComponent }>
             <h1 className={ Styles.header }>о книге</h1>
-            <div className={ Styles.information }>
-                <div className={ Styles.pageCount }>
-                    <div className={ Styles.value }>128</div>
-                    <div className={ Styles.attribute }>стр.</div>
+            <div className={ Styles.mainContent }>
+                <div className={ Styles.information }>
+                    <div className={ Styles.pageCount }>
+                        <div className={ Styles.value1 }>128</div>
+                        <div className={ Styles.attribute1 }>стр.</div>
+                    </div>
+                    <div className={ Styles.editionYear }>
+                        <div className={ Styles.value2 }>2016</div>
+                        <div className={ Styles.attribute2 }>год</div>
+                    </div>
+                    <div className={ Styles.editionCount }>
+                        <div className={ Styles.value3 }>10 000</div>
+                        <div className={ Styles.attribute3 }>тираж (экз.)</div>
+                    </div>
+                    {
+                        BP.isPhoneLandscape(mediaType) ? (
+                            <div className={ Styles.paper }>
+                                <div className={ Styles.value4 }>мягкий</div>
+                                <div className={ Styles.attribute4 }>переплет</div>
+                            </div>
+                        ) : null
+                    }
                 </div>
-                <div className={ Styles.editionYear }>
-                    <div className={ Styles.value }>2016</div>
-                    <div className={ Styles.attribute }>год издания</div>
+                <div className={ Styles.content }>
+                    «Итак, если есть какое утешение во Христе, если есть какая отрада любви,
+                    если есть какое общение духа, если есть какое милосердие и сострадательность» (Флп. 2:1).
+                    Оставалась неделя до Рождества. Ричард Осборн, президент колледжа «Пасифик Юнион».
                 </div>
-                <div className={ Styles.editionCount }>
-                    <div className={ Styles.value }>10 тыс.</div>
-                    <div className={ Styles.attribute }>тираж (экз.)</div>
-                </div>
-                {
-                    BP.isPhoneLandscape(mediaType) ? (
-                        <div className={ Styles.paper }>
-                            <div className={ Styles.value }>мелованная, 80 г/м²</div>
-                            <div className={ Styles.attribute }>бумага, плотность</div>
-                        </div>
-                    ) : null
-                }
-            </div>
-            <div className={ Styles.content }>
-                <p>
-                    Ты прочитаешь в этой книге об одном долларе, который умножился.
-                    О молитве, написанной на воздушном змее. О дверях тюрьмы, которые открылись.
-                    О радиаторе, который сам заработал и баке, который сам заполнился бензином.
-                    Также ты найдешь истории о дожде, который проливался только на огороды миссионерской школы.
-                    О книге, которая не сгорела. О часах, пробивших тринадцать. О манне с небес в Африке.
-                    О поездке внутри торнадо и невидимых руках, державших автомобиль.
-                    О многих таинственных незнакомцах, которых одни люди видели,
-                    а другие нет, и которые всегда защищали, спасали и вызволяли из трудных ситуаций.
-                </p>
             </div>
         </section>
     );
